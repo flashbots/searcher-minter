e7bb28ec22d48afc433ed46b7158cdfc5d9ef525
Scott Bigelow <epheph@gmail.com>
2021-09-06 13:57:31 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Flashbots searcher-minter
+
+This repository contains a very simple demo application of (Flashbots)[https://docs.flashbots.net], which allows arbitrary submission of a single transaction to Flashbots. This could be used for many simple purposes, but in the demonstration, the goal was to mint an NFT.
+
+# Video Live Coding Demo
+
+You can find a walkthrough of Flashbots and the creation of this NFT minting bot here:
+
+[YouTube - Using Flashbots to Mint NFTs on Ethereum - Part 1](https://www.youtube.com/watch?v=1ve1YIpDs_I)
+
+# How to run
+
+Get some [Goerli](https://goerli.etherscan.io/) ETH on a wallet (you'll need a [faucet](https://faucet.goerli.mudit.blog/)). Extract that Goerli wallet's private key (in MetaMask `Account Details -> Export Private Key`), use that value below for `WALLET_PRIVATE_KEY`.
+
+### Note:  It is EXTREMELY dangerous to deal with private keys in this manner, but bots require access to these keys to function. Be careful when using raw private keys that own mainnet ETH or other valuable assets. Keep as little value in these "hot" accounts as possible.
+
+```shell
+npm install
+WALLET_PRIVATE_KEY=0x1d9af4................ npx ts-node src/index.ts
+```
+
+# Where can I learn more?
+
+Check out [docs.flashbots.net](https://docs.flashbots.net).
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { providers, Wallet } from "ethers";
+import { FlashbotsBundleProvider } from "@flashbots/ethers-provider-bundle";
+
+const CHAIN_ID = 5;
+const provider = new providers.InfuraProvider(CHAIN_ID)
+
+const FLASHBOTS_ENDPOINT = "https://relay-goerli.flashbots.net";
+
+if (process.env.WALLET_PRIVATE_KEY === undefined) {
+  console.error("Please provide WALLET_PRIVATE_KEY env")
+  process.exit(1)
+}
+const wallet = new Wallet(process.env.WALLET_PRIVATE_KEY, provider)
+
+// ethers.js can use Bignumber.js class OR the JavaScript-native bigint. I changed this to bigint as it is MUCH easier to deal with
+const GWEI = 10n ** 9n
+const ETHER = 10n ** 18n
+
+async function main() {
+  const flashbotsProvider = await FlashbotsBundleProvider.create(provider, Wallet.createRandom(), FLASHBOTS_ENDPOINT)
+  provider.on('block', async (blockNumber) => {
+    console.log(blockNumber)
+    
+    const bundleSubmitResponse = await flashbotsProvider.sendBundle(
+      [
+        {
+          transaction: {
+            chainId: CHAIN_ID,
+            type: 2,
+            value: ETHER / 100n * 3n,
+            data: "0x1249c58b",
+            maxFeePerGas: GWEI * 3n,
+            maxPriorityFeePerGas: GWEI * 2n,
+            to: "0x20EE855E43A7af19E407E39E5110c2C1Ee41F64D"
+          },
+          signer: wallet
+        }
+      ], blockNumber + 1
+    )
+
+    // By exiting this function (via return) when the type is detected as a "RelayResponseError", TypeScript recognizes bundleSubmitResponse must be a success type object (FlashbotsTransactionResponse) after the if block.
+    if ('error' in bundleSubmitResponse) {
+      console.warn(bundleSubmitResponse.error.message)
+      return
+    }
+
+    console.log(await bundleSubmitResponse.simulate())
+  })
+}
+
+main();
